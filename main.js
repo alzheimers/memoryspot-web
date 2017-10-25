e5f1358af7a8ca34f09588e632166e67fda129fc
shreyas23 <shreyas.niradi@gmail.com>
2017-10-25 09:51:25 -0700

    Initial commit contains completed landing page

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    setTimeout(function () {
+        $('#first').css('visibility','visible').hide().fadeIn('slow');
+    }, 850);
+    setTimeout(function () {
+        $('#second').css('visibility','visible').hide().fadeIn('slow');
+    }, 850);
+    setTimeout(function () {
+        $('#third').css('visibility','visible').hide().fadeIn('slow');
+    }, 2000);
+});
