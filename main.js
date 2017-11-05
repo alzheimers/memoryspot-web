ce8b2ba5b0722b8f8a105f53c110a34d4d6bfa54
Kunal Patel <kunalp925@gmail.com>
2017-11-04 20:41:58 -0700

    initial commits

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
     setTimeout(function () {
-        $('#first').css('visibility','visible').hide().fadeIn('slow');
+        $('#first').css('visibility','visible').hide().fadeIn('normal');
+    }, 850);
+        setTimeout(function () {
+        $('#pageHeaders').css('visibility','visible').hide().fadeIn('fast');
     }, 850);
     setTimeout(function () {
-        $('#second').css('visibility','visible').hide().fadeIn('slow');
+        $('#second').css('visibility','visible').hide().fadeIn('normal');
     }, 850);
     setTimeout(function () {
-        $('#third').css('visibility','visible').hide().fadeIn('slow');
+        $('#third').css('visibility','visible').hide().fadeIn('normal');
     }, 2000);
 });
